ac0633ce6bf4e1ec2494fa0eb7e5d8d199ff8fe4
sedehi <sedehi@users.noreply.github.com>
2022-10-21 12:24:20 +0000

    Fix styling

--- src/Commands/InstallCommand.php
+++ src/Commands/InstallCommand.php
@@ -30,20 +30,19 @@ class InstallCommand extends Command
     public function handle()
     {
         app()->make(Composer::class)->run(['require', 'kodeine/laravel-acl']);
-        Artisan::call('vendor:publish',['--provider' => 'Kodeine\Acl\AclServiceProvider']);
+        Artisan::call('vendor:publish', ['--provider' => 'Kodeine\Acl\AclServiceProvider']);
         app()->make(Composer::class)->run(['require', 'intervention/image']);
-        Artisan::call('vendor:publish',['--provider' => 'Intervention\Image\ImageServiceProviderLaravelRecent']);
+        Artisan::call('vendor:publish', ['--provider' => 'Intervention\Image\ImageServiceProviderLaravelRecent']);
         app()->make(Composer::class)->run(['require', 'sedehi/filterable']);
-        Artisan::call('vendor:publish',['--provider' => 'Sedehi\Filterable\FilterableServiceProvider']);
+        Artisan::call('vendor:publish', ['--provider' => 'Sedehi\Filterable\FilterableServiceProvider']);
         app()->make(Composer::class)->run(['require', 'sedehi/uploadable']);
         app()->make(Composer::class)->run(['require', 'sedehi/laravel-tabler']);
-        Artisan::call('vendor:publish',['--provider' => 'Sedehi\Tabler\TablerServiceProvider']);
-        Artisan::call('vendor:publish',['--provider' => 'Okipa\LaravelFormComponents\LaravelFormComponentsServiceProvider']);
+        Artisan::call('vendor:publish', ['--provider' => 'Sedehi\Tabler\TablerServiceProvider']);
+        Artisan::call('vendor:publish', ['--provider' => 'Okipa\LaravelFormComponents\LaravelFormComponentsServiceProvider']);
         app()->make(Composer::class)->run(['require', 'sedehi/laravel-tools']);
-        Artisan::call('vendor:publish',['--provider' => 'Sedehi\LaravelTools\LaravelToolsServiceProvider']);
+        Artisan::call('vendor:publish', ['--provider' => 'Sedehi\LaravelTools\LaravelToolsServiceProvider']);
         app()->make(Composer::class)->run(['require', 'laravel/horizon']);
-        Artisan::call('vendor:publish',['--provider' => 'Laravel\Horizon\HorizonServiceProvider']);
-        app()->make(Composer::class)->run(['require', 'barryvdh/laravel-debugbar','--dev']);
-
+        Artisan::call('vendor:publish', ['--provider' => 'Laravel\Horizon\HorizonServiceProvider']);
+        app()->make(Composer::class)->run(['require', 'barryvdh/laravel-debugbar', '--dev']);
     }
 }
