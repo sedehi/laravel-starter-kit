99a085b3b50914324a03e8a742a9ba9fd6696e78
Navid Sedehi <navid.sedehi@gmail.com>
2022-10-22 23:10:23 +0330

    wip

--- src/Commands/Questions/ResourceCollection.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Sedehi\LaravelStarterKit\Commands\Questions;
-
-interface ResourceCollection
-{
-}
--- src/LaravelStarterKit.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Sedehi\LaravelStarterKit;
-
-class LaravelStarterKit
-{
-}
